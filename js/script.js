38fb49d1e8d66c393643976b1b344d1a98732f4f
EugeniuPeruzzi <jaka1997@live.it>
2023-06-09 13:54:53 +0200

    implemented structure and vue array

--- js/script.js
+++ js/script.js
@@ -5,7 +5,36 @@ const { createApp } = Vue
 createApp({
   data(){
     return{
-      
+      objsList : [
+        {
+          text : 'Cucinare la pentola',
+          done : false 
+        },
+        {
+          text : 'Lavara i muri',
+          done : false 
+        },
+        {
+          text : 'Aspirare il frigo',
+          done : false 
+        },
+        {
+          text : 'Stirare i tapetti',
+          done : false 
+        },
+        {
+          text : 'Suonare il campanello dei vicini',
+          done : false 
+        },
+        {
+          text : 'Cambiare la lettiera al cocodrilo',
+          done : false 
+        },
+        {
+          text : 'Sistemare gli armadi nei vestiti',
+          done : false 
+        },
+      ]
     }
   }
 }).mount('#app')
